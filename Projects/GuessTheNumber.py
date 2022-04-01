9710246e02c948ab507a15062c67540910fb37e6
PSanthosh12 <99349508+PSanthosh12@users.noreply.github.com>
2022-03-31 19:38:44 -0500

    Finished MadLib.py & compver. of GuessTheNumber.py

--- Projects/GuessTheNumber.py
+++ Projects/GuessTheNumber.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python
 
 #README: This program will work with Python's random module, build functions, work with while loops and conditionals, and get user input
-#Python random module: https://docs.python.org/3/library/random.html
+print()
 
-#COMPUTER VERSION
-#tutorial: https://www.youtube.com/watch?v=8ext9G7xspg&t=414s
+#COMPUTER VERSION: The computer generates a random number that the user is trying to guess
 
+#first we need to import the random module from thePython library for us to use
+import random #random module documentation: https://docs.python.org/3/library/random.html
 
-#USER VERSION
-#tutorial: https://www.youtube.com/watch?v=8ext9G7xspg&t=797s
+#define a function that generates the random integer
+def guess(x):
+    random_number = random.randint(1,x) #the randint(a.b) command is from the random module that generates a random integer within the range (a,b)
+    guess = 0 #just initializing the variable guess so that python does't set it equal to the random_number
+    #create a while loop that takes in user guesses, tells if it's too high, too low, or the user guess correctly
+    while guess != random_number: #if the gues DOES NOT equal the random_number
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        print()
+        if guess < random_number: #if the guess is too low
+            print(f"Sorry {guess} is too low! Guess again.")
+            print()
+        elif guess > random_number: #if the guess is too high
+            print(f"Sorry {guess} is too high! Guess again.")
+            print()
+    print(f"Congrats! You guessed the right number!")
+    print()
 
+#define the parameter for the guess (what x in guess(x) is)
+guess(10) #the computer will generate a number between 1 and 10
+#the user will also be prompted to pick a number between 1 and 10
+
+#The End!
+print("Thank you for playing!")
+print()
+
+#USER VERSION: 
--- Projects/MadLib.py
+++ Projects/MadLib.py
@@ -1,4 +1,50 @@
 #!/usr/bin/python
+print()
 
-#README: This program will get input from the user, work with f-strings, and print a story with the user input
-#tutorial: https://www.youtube.com/watch?v=8ext9G7xspg&t=100s
+#README: This program will take in user input for several parts of speech and give an out of a story containing the user input
+
+#NOTES: string concatenation methods
+#foo = "" #this creates a string variable
+#print("subscribe to" + foo) #basic concatentation between two strings with the + operator
+#print("subscribes to {}".format(foo)) #this will format whatever the value of the variable foo is into the {}
+#print(f"subscribe to {foo}") # f-string that directly lets you insert the variable into the string 
+
+#taking in user input for parts of speech 
+adjective = input("Can you give me an adjective: ")
+nationality = input("Can you give me an nationality: ")
+person = input("Can you give me the name of a person: ")
+noun = input("Can you give me an noun: ")
+adjective2 = input("Can you give me an adjective: ")
+noun2 = input("Can you give me an noun: ")
+adjective3 = input("Can you give me an adjective: ")
+adjective4 = input("Can you give me an adjective: ")
+pluralnoun = input("Can you give me an plural noun: ")
+noun3 = input("Can you give me an noun: ")
+number = input("Can you give me an number: ")
+shapes = input("Can you give me an plural shape: ")
+food = input("Can you give me a type of food: ")
+food2 = input("Can you give me another type of food: ")
+number2 = input("Can you give me another number: ")
+
+#writing out the story using f-string concatenation
+print()
+print("Pizza Pizza MadLib!")
+print()
+#printing one sentence at a time is unnecssary but this way I can keep track of errors easier
+print(f"Pizza was invented by a {adjective} {nationality} chef named {person}.")
+print(f"To make a pizza, you need to take a lump of {noun}, and make a thin, round {adjective2} {noun2}.")
+print(f"Then you cover it with {adjective3} sauce, {adjective4} cheese, and fresh chopped {pluralnoun}.")
+print(f"Next you have to bake it in a very hot {noun3}.")
+print(f"When is tis done, cut it into {number} {shapes}.")
+print(f"Some kids like {food} pissa the best, but my favorite is the {food2} pizza.")
+print(f"If I could, I would eat pizza {number2} times a day!")
+
+#The End!
+print("Thank you for playing!")
+print()
+
+
+
+
+
+
